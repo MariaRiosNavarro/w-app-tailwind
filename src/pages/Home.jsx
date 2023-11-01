a2b9fcd8f0d4f00d9204732b72daf8d22e61c204
SuperCoder <propscode254@gmail.com>
2023-11-01 15:17:15 +0100

    card saved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,13 @@
 const Home = () => {
   return (
     <>
-      <div className="card card-side  shadow-2xl m-10">
-        <figure className="w-30">
-          <img src="https://picsum.photos/seed/picsum/200/300" alt="Movie" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
-          </div>
-        </div>
-      </div>
       <div className="card lg:card-side bg-base-100 shadow-2xl m-10">
-        <figure>
-          <img src="https://picsum.photos/seed/picsum/200/300" alt="Album" />
+        <figure className="h-28 w-full lg:h-auto lg:w-auto">
+          <img
+            className="w-full object-fill lg:w-auto"
+            src="https://picsum.photos/seed/picsum/200/300"
+            alt="Album"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">New album is released!</h2>
